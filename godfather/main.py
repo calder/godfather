fd9281b8f9983e4b0cc72680d8322f91d111472d
Calder Coalson <caldercoalson@gmail.com>
2016-04-28 17:40:33 -0700

    Added logging.

--- godfather/main.py
+++ godfather/main.py
@@ -1,6 +1,7 @@
 import argparse
 import click
 import functools
+import logging
 import mafia
 import os
 
@@ -27,30 +28,44 @@ game = MakeGame(seed=123, players=players, factions=factions, roles=roles)
 """.strip()
 
 def GameDir(*, exists=True):
-  return click.Path(
-    dir_okay=True,
-    file_okay=False,
-    readable=True,
-    writable=True,
-    exists=exists
-  )
+  return click.Path(dir_okay=True,
+                    file_okay=False,
+                    readable=True,
+                    writable=True,
+                    exists=exists)
 
 @click.group()
 def main():
   pass
 
-@main.command()
-@click.option("--game_dir", type=GameDir(exists=False))
+def standard_options(*, game_dir_must_exist=True):
+  def decorator(f):
+    @main.command()
+    @click.option("-v", "--verbose", is_flag=True)
+    @click.option("--game_dir", type=GameDir(exists=game_dir_must_exist))
+    @functools.wraps(f)
+    def wrapper(verbose, *args, **kwargs):
+      level = logging.INFO if verbose else logging.WARNING
+      logging.basicConfig(level=level,
+                          format="%(asctime)s %(message)s",
+                          datefmt="%Y-%m-%d %H:%M:%S:")
+      f(*args, **kwargs)
+    return wrapper
+  return decorator
+
+@standard_options(game_dir_must_exist=False)
 def create(game_dir):
-  # Create the game directory if it doesn't exist.
+  logging.info("Creating %s..." % game_dir)
   os.makedirs(game_dir, exist_ok=True)
 
-  # Create the setup file if it doesn't exist.
   setup_path = os.path.join(game_dir, "setup.py")
-  if not os.path.isfile(setup_path):
+  logging.info("Checking for %s..." % setup_path)
+  if os.path.isfile(setup_path):
+    logging.info("%s already exists." % setup_path)
+  else:
+    logging.info("Creating %s..." % setup_path)
     open(setup_path, "w").write(SETUP_TEMPLATE)
 
-@main.command()
-@click.option("--game_dir", type=GameDir())
+@standard_options()
 def run(game_dir):
   raise NotImplemented
