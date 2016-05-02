1065bd7e0af5410bada6343ad2736702cbd867d1
Calder Coalson <caldercoalson@gmail.com>
2016-05-01 22:40:56 -0700

    Added comments.

--- godfather/moderator.py
+++ godfather/moderator.py
@@ -16,6 +16,7 @@ class Moderator(object):
     self.game.log.on_append(self.event_logged)
 
   def run(self, *, setup_only=False):
+    """Entry point. Blocks until game finishes or an interrupt is received."""
     logging.info("Moderating game...")
     if not self.started:
       self.start()
@@ -23,15 +24,18 @@ class Moderator(object):
       return
 
   def start(self):
+    """Start the game and send out role emails."""
     logging.info("Starting game...")
     self.game.begin()
     self.started = True
     self.save()
 
   def save(self):
+    """Save the current Moderator state to disk."""
     pickle.dump(self, open(self.path, "wb"))
 
   def email(self, to, subject, contents):
+    """Send an email to a list of players, or everyone if to=PUBLIC."""
     assert to
     if to == mafia.events.PUBLIC:
       to = self.game.all_players
@@ -43,6 +47,7 @@ class Moderator(object):
     logging.info("  Contents: %s" % contents)
 
   def event_logged(self, event):
+    """Called when an event is added to the game log."""
     prefix = termcolor.colored(">>>", "yellow")
     logging.info("%s %s" % (prefix, event.colored_str()))
     if event.to:
