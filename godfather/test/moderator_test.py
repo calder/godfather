bae39a3709969ba2991a6ca5c3ad56f0236ec032
Calder Coalson <caldercoalson@gmail.com>
2016-05-01 22:27:25 -0700

    Tested role email sending.

--- godfather/test/moderator_test.py
+++ godfather/test/moderator_test.py
@@ -3,6 +3,7 @@ import pickle
 import pluginbase
 import unittest
 
+from unittest.mock import call, MagicMock
 from .cli_test import *
 from mafia import *
 from ..moderator import *
@@ -13,8 +14,8 @@ class ModeratorTest(CliTest):
     super().setUp()
     self.game   = Game()
     self.town   = self.game.add_faction(Town())
-    self.masons = self.game.add_faction(Masonry("The Fellowship", self.town))
-    self.mafia  = self.game.add_faction(Mafia("The Forces of Darkness"))
+    self.masons = self.game.add_faction(Masonry("Fellowship", self.town))
+    self.mafia  = self.game.add_faction(Mafia("Forces of Darkness"))
     self.frodo  = self.game.add_player("Frodo", Villager(self.masons))
     self.sam    = self.game.add_player("Samwise", Villager(self.masons))
     self.moderator = Moderator(path=self.game_path,
@@ -23,10 +24,14 @@ class ModeratorTest(CliTest):
                                day_end=datetime.time(hour=22))
 
   def test_start(self):
+    self.moderator.email = MagicMock()
+    self.moderator.save  = MagicMock()
     self.moderator.run(setup_only=True)
+
+    assert_equal(self.moderator.email.mock_calls, [
+      call([self.frodo], "Mafia: Start", "You are the Mason Villager."),
+      call([self.sam], "Mafia: Start", "You are the Mason Villager."),
+      call([self.frodo, self.sam], "Mafia: Start", "You are the Fellowship."),
+    ])
+    assert_equal(self.moderator.save.mock_calls, [call()])
     assert self.moderator.started
-    assert_equal(self.game.log, Log([
-      events.RoleAnnouncement(self.frodo, self.frodo.role),
-      events.RoleAnnouncement(self.sam, self.sam.role),
-      events.FactionAnnouncement(self.masons, [self.frodo, self.sam]),
-    ], phase=START))
