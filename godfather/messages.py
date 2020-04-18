dfa8fa1375796614f8518aba2e210507a3d25919
Calder Coalson <caldercoalson@gmail.com>
2020-04-17 22:44:40 -0700

    Remove unnecessary aliasing.

--- godfather/messages.py
+++ godfather/messages.py
@@ -16,16 +16,13 @@ def event_email(event, *, parser):
 @event_email.register(mafia.events.RoleAnnouncement)
 def _(event, *, parser):
   player = event.to[0]
-  abilities = player.role.descriptions
-  commands = parser.get_help(player)
-  objective = player.role.objective
   return render_message(
     "role_announcement.html",
     initial=(event.phase == mafia.START),
     role=event.role,
-    abilities=abilities,
-    commands=commands,
-    objective=objective,
+    abilities=player.role.descriptions,
+    commands=parser.get_help(player),
+    objective=player.role.objective,
   )
 
 def render_death_email(event, *, description):
